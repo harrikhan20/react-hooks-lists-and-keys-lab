4b4bef9bd92193136d421d444db2f74a88d19cae
harris <khanharris7@yahoo.com>
2021-11-24 18:47:39 -0600

    commit tree 57d8a19c10ea8b8d5a89d03c8f92da35b6484aa8

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,12 +1,14 @@
 import React from "react";
+import user from "../data/user";
+import ProjectList from "./ProjectList";
 
 function ProjectItem({ name, about, technologies }) {
   return (
     <div className="project-item">
-      <h3>{name}</h3>
-      <p>{about}</p>
-      <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+      
+      <div className={technologies}>
+        <span>{technologies.name}</span>
+        
       </div>
     </div>
   );
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,15 @@
 import React from "react";
+import user from "../data/user";
 import ProjectItem from "./ProjectItem";
+import App from "./App";
 
 function ProjectList({ projects }) {
   console.log(projects);
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id={projects.name}></div>
+      
     </div>
   );
 }
